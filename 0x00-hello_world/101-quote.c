d9a3a1076c8f6ab588b0d37820e039700a7b2dd7
Ahmed Aboalesaad <ahmed.aboalisaad@gmail.com>
2023-02-21 16:54:34 +0200

    x

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,9 @@
 */
 int main(void)
 {
-	char *one = "and that piece of art is us";
-	char *two = "eful\" - Dora Korpar, 2015-10-19\n";
+	char *one = "and that piece of art is "
+			"useful\" - Dora Korpar, 2015-10-19"
 
-	fprintf(stderr, "%c%c", *one, *two);
+	fprintf(stderr, "%c%c", *one);
 	return (1);
 }
