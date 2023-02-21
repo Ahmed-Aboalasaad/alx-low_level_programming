6111150228e3b579a06718d75d3caaea1f6ac5ff
Ahmed Aboalesaad <ahmed.aboalisaad@gmail.com>
2023-02-21 03:26:37 +0200

    added a simple pringing program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - the main function
+* Return: returns an integer
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
