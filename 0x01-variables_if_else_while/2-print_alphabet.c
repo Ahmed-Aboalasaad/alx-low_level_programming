edb002a02a7f195e1330068ffe3bd4ff49cc0a8a
Ahmed Aboalesaad <ahmed.aboalisaad@gmail.com>
2023-02-25 01:49:47 +0200

    x

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - the main fn
+*Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 79 + 26; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
