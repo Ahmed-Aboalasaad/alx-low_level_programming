68aad94499ab5695f43c6dafe37e000b9b0ddd35
Ahmed Aboalesaad <ahmed.aboalisaad@gmail.com>
2023-02-25 02:44:42 +0200

    x

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - the main fn
+*Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96 ; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
