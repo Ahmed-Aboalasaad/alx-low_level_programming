fc1120808de66d57c1d2fb9d996e02a8192e775a
Ahmed Aboalesaad <ahmed.aboalisaad@gmail.com>
2023-02-27 14:21:20 +0200

    x

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,9 @@
+/**
+ * _abs - caculates the absolute value
+ * @n: the number to calculate its absolute value
+ * Return: the absolute value of the given integer
+ */
+int _abs(int n)
+{
+	return (n > 0 ? n : -n);
+}
