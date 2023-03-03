ced60a78eebd82005dd6af90d8bd057ecdc342bf
Ahmed Aboalesaad <ahmed.aboalisaad@gmail.com>
2023-03-03 15:12:44 +0200

    s

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2,
+ * followed by a new line.
+ * Return: zero when success
+ */
+int main(void)
+{
+	int i;
+	unsigned long long last = 2,
+	beforeLast = 1,
+	new;
+
+	printf("1 2 ");
+	for (i = 0; i < 48; i++)
+	{
+		new = last + beforeLast; /* the new value */
+		beforeLast = last;
+		last = new;
+		printf("%lld ", new);
+	}
+	return (0);
+}
