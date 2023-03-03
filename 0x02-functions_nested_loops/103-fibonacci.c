e4759a68a2dec50c50e8349c24fcab4be91dc456
Ahmed Aboalesaad <ahmed.aboalisaad@gmail.com>
2023-03-03 15:41:57 +0200

    x

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the sum of the even-valued terms of Fibonacci
+ * Sequence followed by a new line
+ * Return: always zero
+ */
+int main(void)
+{
+	unsigned long sum = 0,
+				  last = 2,
+				  beforeLast = 1,
+				  new;
+
+	while (1)
+	{
+		new = last + beforeLast; /* keep the new number */
+		beforeLast = last; /* update the beforeLast number */
+		last = new; /* update the last number */
+
+		if (new > 4000000)
+			break;
+		if (new % 2 == 0)
+			sum += new;
+	}
+	printf("%ld\n", new);
+
+	return (0);
+}
