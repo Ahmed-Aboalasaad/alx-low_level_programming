cd1354738b791e249c16666c258b52416719bd11
Ahmed Aboalesaad <ahmed.aboalisaad@gmail.com>
2023-03-05 15:52:24 +0200

    added _mul() function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - multiplies 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the multiplication of a and b
+*/
+int mul(int a, int b)
+{
+	long result = a * b;
+
+	return (result);
+}
