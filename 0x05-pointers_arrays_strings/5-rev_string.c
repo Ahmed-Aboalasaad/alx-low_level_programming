2d35dc8dc3df202a76e0e0dd139191f9b6ed68bd
Ahmed Aboalesaad <ahmed.aboalisaad@gmail.com>
2023-03-07 13:02:59 +0200

    tweaks in 5-rev_string.c file

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -18,25 +18,25 @@ void rev_string(char *s)
 	}
 
 	/* I'm using the same metod of substituting 2 integers without third one */
+
+	/* add the corresponding form right side to the left side */
+	for (i = 0; i < length / 2; i++)
+		s[i] += s[length - 1 - i];
+
+	/*
+	 * replace corresponding right with (left - right)
+	 * different in odd than even
+	 */
 	if (length % 2 == 0)
-	{
-		/* add the corresponding form right side to the left side */
-		for (i = 0; i < length / 2; i++)
-			s[i] += s[length - 1 - i];
+		for (i = length / 2, j = (length / 2) - 1; i < length; i++, j--)
+			s[i] = s[j] - s[i];
+	else
+		for (i = (length / 2) + 1, j = (length / 2) - 1; i < length; i++, j--)
+			s[i] = s[j] - s[i];
 
-		/*
-		 * replace corresponding right with (left - right)
-		 * different in odd than even
-		 */
-		if (length % 2 == 0)
-			for (i = length / 2, j = (length / 2) - 1; i < length; i++, j--)
-				s[i] = s[j] - s[i];
-		else
-			for (i = (length / 2) + 1, j = (length / 2) - 1; i < length; i++, j--)
-				s[i] = s[j] - s[i];
-
-		/* replace corresponding left with (left - right) */
-		for (i = 0; i < length / 2; i++)
-			s[i] -= s[length - 1 - i];
-	}
+	/* replace corresponding left with (left - right) */
+	for (i = 0; i < length / 2; i++)
+		s[i] -= s[length - 1 - i];
 }
+
+
