d52704987a0e1cfb9781db8d51e7fa46a5a5bbd3
Ahmed Aboalesaad <ahmed.aboalisaad@gmail.com>
2023-03-07 21:43:53 +0200

    added 0-strcat.c, README.md, main.h

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: pointer to the destination
+ * @src: pointer to the src
+ */
+char *_strcat(char *dest, char *src)
+{
+	int destLength, srcLength, i;
+
+	destLength = 0;
+	while (1)
+	{
+		if (!dest[destLength])
+			break;
+		destLength++;
+	}
+
+	srcLength = 0;
+	while (1)
+	{
+		if (!src[srcLength])
+			break;
+		srcLength++;
+	}
+
+	dest += destLength;
+	for (i = 0; src[i]; i++)
+	{
+		*(dest + i) = src[i];
+	}
+
+	dest += srcLength;
+	*dest = '\0';
+
+	dest -= destLength;
+	dest -= srcLength;
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Here you can find my tasks on the second project about pointers, arrays, strings
