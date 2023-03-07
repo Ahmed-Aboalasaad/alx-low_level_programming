579cde2a28bf4e578580373300f746e5c1c79f6c
Ahmed Aboalesaad <ahmed.aboalisaad@gmail.com>
2023-03-07 13:39:14 +0200

    added 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,7 +17,7 @@ void puts_half(char *str)
 		length++;
 	}
 
-	n = (length % 2 == 0) ? length / 2 : (length - 1) / 2;
+	n = (length % 2 == 0) ? (length / 2) : (length - 1) / 2;
 	for (i = n; str[i]; i++)
 	{
 		_putchar(str[i]);
