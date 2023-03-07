79cf4c079f854377f6ea553f8cd5410b16677fd8
Ahmed Aboalesaad <ahmed.aboalisaad@gmail.com>
2023-03-07 23:02:21 +0200

    x

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: the string pointer
+ * Return: pointer to the capitalized string
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] >= 97 && s[i] < 122)
+			s[i] -= 32;
+		else
+			continue;
+	}
+	return (s);
+}
