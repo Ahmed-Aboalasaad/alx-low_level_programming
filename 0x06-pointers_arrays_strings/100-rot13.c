c6a64c4303a6b9419050ed6a7343b3d6985d38fa
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-07-01 20:00:35 +0200

    fixed 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,14 +7,14 @@
  */
 char *rot13(char *s)
 {
-	int i;
+	int i, increase, decrease;
 
 	for (i = 0; s[i]; i++)
 	{
-		if ((s[i] >= 'A' && s[i] <= 'M') || (s[i] >= 'a' && s[i] <= 'm'))
-			s[i] += 13;
-		else if ((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
-			s[i] -= 13;
+		increase = (s[i] >= 'A' && s[i] <= 'M') || (s[i] >= 'a' && s[i] <= 'm');
+		decrease = (s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z');
+		if ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] = s[i] + increase * 13 - decrease * 13;
 	}
 
 	return (s);
