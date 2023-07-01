dc0481f9571cdda9f4376f1ceed6c95c268b8ac6
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-07-01 20:14:09 +0200

    fixed 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,8 +7,20 @@
  */
 char *leet(char *s)
 {
-	int i;
-	
+	int i, isA, isE, isO, isT, isL, isLeet;
+
+	for (i = 0; s[i]; i++)
+	{
+		isA = s[i] == 'A' || s[i] == 'a';
+		isE = s[i] == 'E' || s[i] == 'e';
+		isO = s[i] == 'O' || s[i] == 'o';
+		isT = s[i] == 'T' || s[i] == 't';
+		isL = s[i] == 'L' || s[i] == 'l';
+		isLeet = isA || isE || isO || isT || isL;
+
+		if (isLeet)
+			s[i] = isA * '4' + isE * '3' + isO * '0' + isT * '7' + isL * '1';
+	}
 	return (s);
 }
 
