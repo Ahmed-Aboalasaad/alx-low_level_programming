bf97b39b6f0687a97121106d105c14fdc34f6ab5
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-07-03 01:54:22 +0200

    added 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+/**
+ * pow - power function
+ * @b: base
+ * @p: power
+ *
+ * Return: the base to the power
+ */
+int pow_int(int b, int p)
+{
+	int tmp = b;
+
+	if (p == 0)
+		return 1;
+	if (p == 1)
+		return b;
+
+	while (p > 1)
+	{
+		b *= tmp;
+		p--;
+	}
+	return b;
+}
+
+/**
+ * print_number - Prints a number
+ * @n: the number to be printed
+ *
+ * Retrun: void
+ */
+void print_number(int n)
+{
+	int copy = n, digitCount = 0;
+
+	/* handle zeroes */
+	if (n == 0)
+	{
+		_putchar(48);
+		return;
+	}
+
+	/* handle negative values */
+	if (n < 0)
+	{
+		_putchar(45);
+		n = -n;
+		copy = n;
+	}
+
+	/* count the digits */
+	while (copy > 0)
+	{
+		copy /= 10;
+		digitCount++;
+	}
+
+	/* print the number */
+	for (; digitCount > 0; digitCount--)
+	{
+		copy = n;
+		copy /= pow_int(10, digitCount - 1);
+		_putchar(copy % 10 + 48);
+	}
+
+}
+
