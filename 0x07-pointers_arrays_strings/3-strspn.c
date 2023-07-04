cc8fe7bd48f43bfe74d5cac67961a471979e14b3
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-07-04 04:31:01 +0200

    added 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+
+/**
+ * _strspn - gets the length of a prefix substring
+ *
+ * @s: the string to search in
+ * @accept: the accepted characters
+ * Return: the number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, found;
+
+	for (i = 0; s[i]; i++)
+	{
+		found = 0;
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				found = 1;
+				break;
+			}
+		}
+		if (!found)
+			break;
+	}
+
+	return (i);
+}
