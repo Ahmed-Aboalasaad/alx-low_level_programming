5a915f2e928b0525893a6d7586b15732404a0ef2
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-07-21 14:45:49 +0200

    added README.md and 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums the given integers except the first one
+ *
+ * @n: the number of the parameters to be given
+ * Return: the sum
+ */
+int sum_them_all(int n, ...)
+{
+	int sum = 0, i;
+
+	/* make a variable of va_list type to hold the list of arguments */
+	va_list args;
+
+	/* Start iterating them */
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+		/* va_arg(args, int) -> the next argument */
+		sum += va_arg(args, int);
+
+	/* free the arguments list */
+	va_end(args);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This is the directory of the variadic functions project
