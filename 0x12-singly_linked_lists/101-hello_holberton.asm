09a1b15962c6c572fd6ce451956316aaff097c79
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-07-29 13:15:57 +0200

    trial of task #6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,1 +1,16 @@
- a 64-bit program in assembly that prints Hello, Holberton, followed by a new line
+section .data
+    hello db 'Hello, Holberton', 10, 0
+section .text
+    global _start
+    extern printf
+_start:
+    ; call printf
+    push rbp
+    mov rbp, rsp
+    mov edi, hello
+    xor eax, eax
+    call printf
+    ; exit program
+    xor edi, edi
+    mov eax, 60
+    syscall
