edc777968ef483f7547f882cb1e2411cda2a74aa
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-08-09 13:55:56 +0200

    task #3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
 
@@ -10,8 +11,17 @@
  */
 int main(int argc, char *argv[])
 {
-	(void)argv;
-	printf("%d\n", argc);
+	int x, y;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (0);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	printf("%d\n", x * y);
 
 	return (0);
 }
