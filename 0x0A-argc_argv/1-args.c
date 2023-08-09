c0673d63b05ddebea84ed0c53e7935067fdc7d96
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-08-09 11:29:58 +0200

    task #1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - starting point
+ *
+ * @argc: counts arguments
+ * @argv: arguments vector
+ * Return: 0 when succeeds
+ */
+int main(int argc, char *argv[])
+{
+    (void)argv;
+    printf("%d\n", argc);
+
+    return (0);
+}
