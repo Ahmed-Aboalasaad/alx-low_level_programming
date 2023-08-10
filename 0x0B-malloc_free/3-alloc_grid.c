fc8d01920d9ef696df63520aa9be5e76c0d53e15
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-08-10 09:41:20 +0200

    Hot Fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,7 +7,7 @@
  *
  * @width: grid width
  * @height: grid height
- * Return: Null on failure or zero height or width..
+ * Return: Null on failure or zero/negtive height or width..
  * otherwise, pointer to the grid
  */
 int **alloc_grid(int width, int height)
@@ -15,18 +15,19 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **grid;
 
-	if (width == 0 || height == 0)
+	/* check for correct aspects */
+	if (width < 1 || height < 1)
 		return (NULL);
 
-	/* allocation */
-	grid = (int **)malloc(height * sizeof(int));
+	/* allocation & failure check */
+	grid = (int **)malloc(height * sizeof(int *));
 	if (grid == NULL)
 		return (NULL);
 
-	/* further allocation & initialization */
+	/* further allocation, failure check & initialization */
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int *)malloc(width * sizeof(int *));
+		grid[i] = (int *)malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 			return (NULL);
 		for (j = 0; j < width; j++)
