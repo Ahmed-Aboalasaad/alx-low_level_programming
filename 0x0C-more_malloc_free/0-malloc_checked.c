cc31ed2ca3b95e5c2f749cab721d004626da76ea
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-08-11 14:55:01 +0200

    more malloc, free initial files & task #0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * exits with status 98 when it fails
+ *
+ * @b: number of bytes to allocate
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *tmp = malloc(b * sizeof(char));
+
+	if (tmp == NULL)
+		exit(1);
+
+	return (tmp);
+}
