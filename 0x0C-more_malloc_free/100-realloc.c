56bd42fd08c7452a1111c40353456e37e7b2a095
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-08-11 19:22:30 +0200

    Task #4 (advanced #1)

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * min - minimum
+ *
+ * @x: first
+ * @y: second
+ * Return: the minimum of them
+ */
+unsigned int min(unsigned int x, unsigned int y)
+{
+    return ((x > y) ? x : y);
+}
+
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ *
+ * The contents will be copied to the newly allocated space, in the range
+ * from the start of ptr up to the minimum of the old and new sizes
+ *
+ * @ptr: a pointer to the memory previously allocated with malloc(old_size)
+ * @old_size: the size, in bytes, of the allocated space for ptr
+ * @new_size: and new_size is
+ * Return: NULL when new_size=0 and ptr != NULL (acting as free)
+ * and when it fails
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+    void *new;
+    unsigned int i;
+
+    if (new_size == old_size)
+        return (ptr);
+
+    /* Act as malloc */
+    if (ptr == NULL)
+    {
+        new = malloc(new_size);
+        if (new != NULL)
+            free(ptr);
+        return (new);
+    }
+
+    /* Act as free */
+    if (new_size == 0)
+    {
+        free(ptr);
+        return (NULL);
+    }
+
+    /* Allocate & copy contents */
+    new = malloc(new_size);
+    if (new == NULL)
+        return (NULL);
+
+    for (i = 0; i < min(old_size, new_size); i++)
+        *((char *)new + i) = *((char *)ptr + i);
+    return ((void *)new);
+}
