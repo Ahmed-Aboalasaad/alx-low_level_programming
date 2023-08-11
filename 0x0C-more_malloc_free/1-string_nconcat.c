ca8dde2538d4030f48c7507d450d8f092f492604
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-08-11 15:20:19 +0200

    Task #1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,36 +15,36 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    int i, size1 = 0, size2 = 0;
-    char *str;
+	unsigned int i, size1 = 0, size2 = 0;
+	char *str;
 
-    /* validate s1 & s2 */
-    if (!s1)
-        s1 = "";
-    if (!s2)
-        s2 = "";
+	/* validate s1 & s2 */
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
 
-    /* find the size */
-    for (i = 0; s1[i]; i++)
-        size1++;
-    for (i = 0; s2[i]; i++)
-        size2++;
+	/* find the size */
+	for (i = 0; s1[i]; i++)
+		size1++;
+	for (i = 0; s2[i]; i++)
+		size2++;
 
-    /* validate right n */
-    if (n > size2)
-        n = size2;
+	/* validate right n */
+	if (n > size2)
+		n = size2;
 
-    /* Allocation & failure check */
-    str = malloc((size1 + n + 1) * sizeof(char));
-    if (!str)
-        return (NULL);
+	/* Allocation & failure check */
+	str = malloc((size1 + n + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
 
-    /* Concatenation */
-    for (i = 0; i < size1; i++)
-        str[i] = s1[i];
-    for (i = size1; i < size1 + n; i++)
-        str[i] = s2[i - size1];
-    str[size1 + n] = '\0';
+	/* Concatenation */
+	for (i = 0; i < size1; i++)
+		str[i] = s1[i];
+	for (i = size1; i < size1 + n; i++)
+		str[i] = s2[i - size1];
+	str[size1 + n] = '\0';
 
-    return (str);
+	return (str);
 }
