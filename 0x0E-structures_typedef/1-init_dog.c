da47de0c52d61d8875a0287617c1026e23449ff5
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-08-15 00:07:13 +0200

    Hot Fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,6 +12,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL || name == NULL || owner == NULL)
+		return;
+
 	d->age = age;
 	d->name = name;
 	d->owner = owner;
