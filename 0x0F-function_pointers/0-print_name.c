aad82a367ed7384143a875790b78824d16b34a62
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-08-17 00:40:00 +0200

    Hot Fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,8 +3,12 @@
 
 /**
  * print_name - prints a name
+ *
+ * @name: the name to print
+ * @f: a pointer function to the printing logic
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
+	f(name);
 }
