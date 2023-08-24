bf3542b5759200dd2e2d3469c293b1678f7031f9
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-08-25 00:39:28 +0300

    Hot Fix

--- 0x12-singly_linked_lists/4-main.c
+++ 0x12-singly_linked_lists/4-main.c
@@ -10,18 +10,18 @@
  */
 int main(void)
 {
-    list_t *head;
+	list_t *head;
 
-    head = NULL;
-    add_node_end(&head, "Bob");
-    add_node_end(&head, "&");
-    add_node_end(&head, "Kris");
-    add_node_end(&head, "love");
-    add_node_end(&head, "asm");
-    print_list(head);
-    free_list(head);
-    printf("freed\n");
-    /* print_list(head); */
-    head = NULL;
-    return (0);
+	head = NULL;
+	add_node_end(&head, "Bob");
+	add_node_end(&head, "&");
+	add_node_end(&head, "Kris");
+	add_node_end(&head, "love");
+	add_node_end(&head, "asm");
+	print_list(head);
+	free_list(head);
+	printf("freed\n");
+	/* print_list(head); */
+	head = NULL;
+	return (0);
 }
