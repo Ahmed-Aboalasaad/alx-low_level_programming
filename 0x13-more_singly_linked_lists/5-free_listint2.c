5d6e665af7d10b67fd21a3339314028d90e6614f
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-08-29 17:54:26 +0300

    Task #5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ *
+ * @head: the list head
+ * Return: nothing
+*/
+void free_listint2(listint_t **head)
+{
+    listint_t *last, *before_last;
+
+	if (!head)
+		return;
+
+	while (head)
+	{
+		last = head;
+		while (last->next)
+		{
+			before_last = last;
+			last = last->next;
+		}
+		before_last->next = NULL;
+		free(last);
+		if (last == before_last)
+			head = NULL;
+	}
+}
