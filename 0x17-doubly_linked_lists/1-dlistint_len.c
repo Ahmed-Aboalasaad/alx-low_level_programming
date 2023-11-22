69eeed5ca158845935dac5e077104135cc9d93b7
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-11-22 11:37:14 +0200

    Task #1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns #elements in a linked dlistint_t list
+ *
+ * @h: the list head
+ * Return: #elements in a linked dlistint_t list
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+    int counter;
+
+    if (!h)
+        return (0);
+    for (counter = 0; h; h = h->next)
+        counter++;
+    return (counter);
+}
