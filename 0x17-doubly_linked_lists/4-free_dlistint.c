fe6a97ee59129b3bbcd8b36bf0e0174f98d1bc93
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-11-22 14:02:39 +0200

    Task #4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ *
+ * @head: the list head
+ * Return: nothing
+*/
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *tmp;
+    if (!head)
+        return;
+    for (tmp = head; head->next; head = tmp)
+    {
+        tmp = head->next;
+        free(head);
+    }
+    free(head);
+}
