ca35cf38c0a6781a2cddbe5727c8f53ffd84aa0e
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-11-22 11:33:22 +0200

    Hot Fix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,11 +8,11 @@
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-    int counter;
+	int counter;
 
-    if (!h)
-        return (0);
-    for (counter = 0; h; h = h->next, counter++)
-        printf("%d\n", h->n);
-    return (counter);
+	if (!h)
+		return (0);
+	for (counter = 0; h; h = h->next, counter++)
+		printf("%d\n", h->n);
+	return (counter);
 }
