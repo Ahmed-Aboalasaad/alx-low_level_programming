d96a862170d35ad06206dfca58455b883e08ae57
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-11-26 10:19:47 +0200

    Task #2

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,24 @@
+int add(int x, int y)
+{
+    return (x + y);
+}
+
+int sub(int x, int y)
+{
+    return (x - y);
+}
+
+int mul(int x, int y)
+{
+    return (x * y);
+}
+
+float div(int x, int y)
+{
+    return ((y == 0) ? 0.0 : (x / y));
+}
+
+int mod(int x, int y)
+{
+    return (x % y);
+}
