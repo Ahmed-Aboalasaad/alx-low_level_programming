2230a248906d9d3331b27d71f0856c0751c18cae
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-12-27 13:13:17 +0200

    inital files for Hash Tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ *
+ * @size: the table size
+ * Return: a pointer to the created hash table,
+ * or NULL if something wrong happened.
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+}
+
+// make the key unique
